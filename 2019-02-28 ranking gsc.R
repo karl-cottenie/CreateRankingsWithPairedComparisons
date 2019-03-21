8e8d466c4a430befe0b009fc1c5c0a0112246875
Karl Cottenie <karl.cottenie@gmail.com>
2019-03-21 16:01:05 -0400

    2019-03-21 Create ranking w/ Bradley Terry algorithm
    works with sparse matrices

--- 2019-02-28 ranking gsc.R
+++ 2019-02-28 ranking gsc.R
@@ -12,6 +12,8 @@ library(viridis)
 # + scale_color/fill_viridis(discrete = T/F)
 theme_set(theme_light())
 library(statnet)
+library(BradleyTerryScalable)
+library(igraph)
 
 # Startup ends here
 
@@ -25,15 +27,17 @@ library(statnet)
 # TODO items #TODO
 # names for data frames (dfName), for lists (lsName), for vectors (vcName) (Thanks Jacqueline May)
 
-## Sources ------
+## _Sources ------
 # https://www.anishathalye.com/2015/03/07/designing-a-better-judging-system/
 # https://cvxr.rbind.io/
+# https://www.anishathalye.com/2015/11/09/implementing-a-scalable-judging-system/
+# https://cran.r-project.org/web/packages/BradleyTerryScalable/index.html
+# https://cran.r-project.org/web/packages/BradleyTerry2/vignettes/BradleyTerry.pdf
 
-## Input example from the website -----
-dfComparison = read_table("https://www.anishathalye.com/media/2015/03/07/blueprint-rookie-data.txt", comment = "#", col_names = c("Team1", "Team2", "Team1Better"))
-# TODO check w/ Gabe if my label of the third column is correct, or Team2Better?
+## _Input example from the website -----
+dfComparison = read_table("https://www.anishathalye.com/media/2015/03/07/blueprint-rookie-data.txt", comment = "#", col_names = c("Team1", "Team2", "Team2Better"))
 
-## Create S matrix - Method 1 -----
+## (>_<) __Create S matrix - Method 1 -----
 # rows and colums are the teams, entries count the number of times the row team was
 # preferred over the column team
 
@@ -41,26 +45,27 @@ dfComparison = read_table("https://www.anishathalye.com/media/2015/03/07/bluepri
 # to make computation easier, for all rows w/ a false comparison, switch Team1 and Team2
 # followed by counting the number of combinations per unique pair
 
-dfS = bind_rows(dfComparison %>%
-                  filter(Team1Better == FALSE) %>% # select the FALSE rows
-                  mutate(Team1S = Team2,           # switch team identity
-                         Team2S = Team1),
-                dfComparison %>% filter(Team1Better == TRUE) %>%
-                  mutate(Team1S = Team1,
-                         Team2S = Team2)) %>%     # bind it w/ the TRUE rows
-  group_by(Team1S, Team2S) %>%                    # create unique pairs
-  summarise(TotalBetter = n()) %>%                # count the total number
-  spread(Team2S, TotalBetter) %>%                 # create the wide format
-  replace(is.na(.), 0)                            # clean up
-#==> Not a square matrix, what could be wrong?
-
-"%w/o%" <- function(x, y) x[!x %in% y] #--  x without y
-
-names(dfS[,-1]) %w/o% dfS$Team1S
-dfS$Team1S %w/o% names(dfS[,-1])
+# dfS = bind_rows(dfComparison %>%
+#                   filter(Team2Better == FALSE) %>% # select the FALSE rows
+#                   mutate(Team1S = Team2,           # switch team identity
+#                          Team2S = Team1),
+#                 dfComparison %>% filter(Team2Better == TRUE) %>%
+#                   mutate(Team1S = Team1,
+#                          Team2S = Team2)) %>%     # bind it w/ the TRUE rows
+#   group_by(Team1S, Team2S) %>%                    # create unique pairs
+#   summarise(TotalBetter = n()) %>%                # count the total number
+#   spread(Team2S, TotalBetter) %>%                 # create the wide format
+#   replace(is.na(.), 0)                            # clean up
+# #==> Not a square matrix, what could be wrong?
+# 
+# "%w/o%" <- function(x, y) x[!x %in% y] #--  x without y
+# 
+# names(dfS[,-1]) %w/o% dfS$Team1S
+# dfS$Team1S %w/o% names(dfS[,-1])
 #==> certain Teams were always ranked worse/better, so do not show up in column/row!
+#==> do not use this method
 
-## Create S matrix - Method 2 ------
+## __Create S matrix - Method 2 ------
 
 ## Step 1: create unique team names
 vcNodes = dfComparison %>% select(Team1, Team2) %>%
@@ -75,7 +80,7 @@ dfComparisonNumber =  bind_cols(dfComparison,
                                    left_join(vcNodes, by = c("Team1" = "vcNodes"))),
                                 (dfComparison %>% select(Team2) %>%
                                    left_join(vcNodes, by = c("Team2" = "vcNodes")))) %>%
-  select(number, number1, Team1Better) %>%
+  select(number, number1, Team2Better) %>%
   rename(Team1Number = number,
          Team2Number = number1) %>%
   mutate(row = 1:nrow(dfComparison)) %>%
@@ -86,7 +91,7 @@ dfS = array(0, dim = c(nrow(vcNodes), nrow(vcNodes), nrow(dfComparison)))
 
 ## Step 4: fill empty array the smart way
 for (i in 1:nrow(dfComparisonNumber)) {
-  if (dfComparisonNumber[i,3] == TRUE){
+  if (dfComparisonNumber[i,3] == FALSE){
     dfS[dfComparisonNumber[i,1], dfComparisonNumber[i,2], dfComparisonNumber[i,4]] = 1
   } else {
     dfS[dfComparisonNumber[i,2], dfComparisonNumber[i,1], dfComparisonNumber[i,4]] = 1
@@ -96,14 +101,14 @@ for (i in 1:nrow(dfComparisonNumber)) {
 ## Step 5: compute the sum of all the comparisons per team
 dfS = apply(dfS, c(1,2), sum)
 
-## sanity check1 ----
+## __sanity check1 ----
 # rowsums should equal frequency of each team in Team1 column and TRUE
 # PLUS frequency in Team2 column and FALSE
 
-full_join(dfComparison %>% filter(Team1Better == TRUE) %>%
+full_join(dfComparison %>% dplyr::filter(Team2Better == FALSE) %>%
             group_by(Team1) %>%
             summarise(n1 = n()),
-          dfComparison %>% filter(Team1Better == FALSE) %>%
+          dfComparison %>% dplyr::filter(Team2Better == TRUE) %>%
             group_by(Team2) %>%
             summarise(n2 = n()) %>%
             dplyr::rename(Team1 = Team2)) %>%
@@ -117,13 +122,14 @@ full_join(dfComparison %>% filter(Team1Better == TRUE) %>%
 
 rowSums(dfS)
 
-## sanity check2: colsums should equal frequency of each team in Team1 column and FALSE
+## __sanity check2: ------
+## colsums should equal frequency of each team in Team1 column and FALSE
 ## PLUS frequency in Team2 column and TRUE
 
-(full_join(dfComparison %>% filter(Team1Better == FALSE) %>%
+(full_join(dfComparison %>% dplyr::filter(Team2Better == TRUE) %>%
             group_by(Team1) %>%
             summarise(n1 = n()),
-          dfComparison %>% filter(Team1Better == TRUE) %>%
+          dfComparison %>% dplyr::filter(Team2Better == FALSE) %>%
             group_by(Team2) %>%
             summarise(n2 = n()) %>%
             dplyr::rename(Team1 = Team2)) %>%
@@ -140,7 +146,7 @@ colSums(dfS)
 #==> all elements correspond, except for the 2 0's in the colSums(dfS)
 # correspond to the teams that always won
 
-## Visualize the S matrix -------
+## _Visualize the S matrix -------
 
 judging = as.network(x = dfS,
                      directed = TRUE,
@@ -149,6 +155,45 @@ judging = as.network(x = dfS,
 
 plot.network(judging)
 
-#TODO: create the convex optimization functions
+## _BradleyTerryScalable approach --------
+
+## __preparing the data from the raw counts ------
+dfComparison_4col = codes_to_counts(dfComparison, c("FALSE","TRUE"))
+dfComparison_btdata = btdata(dfComparison_4col, return_graph = T)
+
+par(mar = c(0, 0, 0, 0) + 0.1)
+plot.igraph(dfComparison_btdata$graph, edge.arrow.size = 0.5)
+
+summary(dfComparison_btdata)
+
+## __Fitting the model -------
+Comparisons_fit = btfit(dfComparison_btdata, 1.1)
+coef(Comparisons_fit)
+#==> very similar to the results reported in the original data set
+# https://www.anishathalye.com/media/2015/03/07/blueprint-rookie-results.txt
+#==> method is similar enough
+
+## (>_<) Fitting the model from the S matrix
+# 
+# dfS %>% btdata(return_graph = T) %>% 
+#   btfit(1.1) %>% 
+#   coef()
+#==> identical to the coef(Comparsions_fit) above, so yay!
+#==> not use this approach
+
+## __Ploting the model results -----
+
+summary(Comparisons_fit, SE = T) %>% .$item_summary %>% 
+  # add the total wins and losses bc more intuitive
+  left_join(tibble(item = vcNodes$vcNodes,
+                   TotalWins = rowSums(dfS),
+                   TotalLosses = colSums(dfS))) %>% 
+  ggplot(aes(x = fct_reorder(item, desc(estimate)), y = estimate, 
+             ymin = estimate-SE, ymax = estimate + SE)) + 
+  geom_pointrange() + xlab("Item") + ylab("Rank") +
+  # TotalWins at the top of the figure
+  geom_text(aes(x = fct_reorder(item, desc(estimate)), y = 4, label = TotalWins)) +
+  # TotalLosses at the bottom of the figure
+  geom_text(aes(x = fct_reorder(item, desc(estimate)), y = -10, label = TotalLosses))
 
 
